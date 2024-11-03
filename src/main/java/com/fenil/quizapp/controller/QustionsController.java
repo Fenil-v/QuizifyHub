210ecdd2c8a4520598a2ab1752bbbbb0cf05ffc7
Fenil-Mansukhbhai-Vaghasiya <vaghasiya.f@northeastern.edu>
2024-11-03 11:56:25 -0500

    created controller for fetching all data

---
+++ src/main/java/com/fenil/quizapp/controller/QustionsController.java
@@ -0,0 +1,23 @@
+package com.fenil.quizapp.controller;
+
+import com.fenil.quizapp.Question;
+import com.fenil.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QustionsController {
+
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+}
---
+++ src/main/java/com/fenil/quizapp/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.fenil.quizapp.service;
+
+import com.fenil.quizapp.Dao.QuestionDao;
+import com.fenil.quizapp.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+    public List<Question>getAllQuestions(){
+        return questionDao.findAll();
+    }
+}
