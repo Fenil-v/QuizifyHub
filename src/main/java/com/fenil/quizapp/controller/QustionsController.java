79a400367ad0773e07ca80644dc9c86f125d6d95
Fenil-Mansukhbhai-Vaghasiya <vaghasiya.f@northeastern.edu>
2024-11-03 12:25:54 -0500

    create method for feteching data by sprcified category

--- src/main/java/com/fenil/quizapp/controller/QustionsController.java
+++ src/main/java/com/fenil/quizapp/controller/QustionsController.java
@@ -4,6 +4,7 @@ import com.fenil.quizapp.Question;
 import com.fenil.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,16 @@ public class QustionsController {
 
     @Autowired
     QuestionService questionService;
+
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
+
+    @GetMapping("category/{category}")
+    public List<Question> getAllQuestionsByCategory(@PathVariable String category){
+            return questionService.getAllQuestionsByCategory(category);
+    }
+
 
 }
